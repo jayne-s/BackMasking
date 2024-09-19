fa5f0924d3f7d1822655ed5c3ef1e9e402c3a5f8
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2024-09-19 12:08:05 -0500

    Update ListStackNode.java

--- ListStackNode.java
+++ ListStackNode.java
@@ -1,9 +1,10 @@
 /**
  * Implementation of the Node class which creates an object of type ListStackNode
  * that contains a double and two pointers, next and prev. The constructor is invoked
- * each time a new node is added to the list through the use of the push() method in the
- * ListStack.java file. When multiple ListStackNodes are connected to one another using
+ * each time a new node is added to the list through the use of the push() method. 
+ * When multiple ListStackNodes are connected to one another using
  * the corresponding next and prev pointers, they form a doubly linked list.
+ * Time Complexity: O(1)
  */
 
 public class ListStackNode {
