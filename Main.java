2650606677ad4a6d1354d8185fb65adf2a889ed9
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2024-09-21 21:30:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,15 +19,15 @@ public class Main {
     //    arr.printItems();
 
         
-       ListStack list = new ListStack();
-       list.push(1.2);
-       list.push(2.3);
-       list.push(3.4);
-       list.push(4.5);
-       list.push(5.6);
-       while(!list.isEmpty()){
-        System.out.println(list.pop());
-       }
+    //    ListStack list = new ListStack();
+    //    list.push(1.2);
+    //    list.push(2.3);
+    //    list.push(3.4);
+    //    list.push(4.5);
+    //    list.push(5.6);
+    //    while(!list.isEmpty()){
+    //     System.out.println(list.pop());
+    //    }
 
     //    System.out.println("count: " + list.count());
     //    list.printItems();
